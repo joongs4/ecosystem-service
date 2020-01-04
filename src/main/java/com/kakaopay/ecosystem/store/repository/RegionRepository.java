2ee95c1747352298cb2acd491839f299a43f1284
joonggeon <joongs4@naver.com>
2020-01-04 16:39:04 +0900

    RegionRepository - solved SELECT N+1 Problem

--- src/main/java/com/kakaopay/ecosystem/store/repository/RegionRepository.java
+++ src/main/java/com/kakaopay/ecosystem/store/repository/RegionRepository.java
@@ -2,13 +2,16 @@ package com.kakaopay.ecosystem.store.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kakaopay.ecosystem.store.jpo.RegionJpo;
 
 public interface RegionRepository extends JpaRepository<RegionJpo, String> {
 
+	@EntityGraph(attributePaths = "ecosystemServices")
 	List<RegionJpo> findByNameContaining(String name);
 
+	@EntityGraph(attributePaths = "ecosystemServices")
 	List<RegionJpo> findByNameEndsWith(String name);
 }
